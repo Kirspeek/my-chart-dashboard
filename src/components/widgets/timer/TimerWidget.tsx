74bad4882676bdbcd21ccab3d717206ffaf34ca8
Kirspeek <kira.cherepenko@gmail.com>
2025-08-04 20:42:41 +0200

    timer coming soon

--- src/components/widgets/timer/TimerWidget.tsx
+++ src/components/widgets/timer/TimerWidget.tsx
@@ -1,59 +1,33 @@
 "use client";
 
-import React, { useRef } from "react";
+import React from "react";
 import WidgetBase from "../../common/WidgetBase";
 import { TimerWidgetProps } from "../../../../interfaces/components";
-import { useTimerLogic } from "@/hooks/useTimerLogic";
-import TimerCircle from "./TimerCircle";
-import TimerControls from "./TimerControls";
 
 export default function TimerWidget({ className = "" }: TimerWidgetProps) {
-  const svgRef = useRef<SVGSVGElement>(null);
-  const {
-    duration,
-    timeLeft,
-    isRunning,
-    isPaused,
-    dragging,
-    previewDuration,
-    toggleTimer,
-    resetTimer,
-    onPointerDown,
-    formatTime,
-  } = useTimerLogic();
-
-  // Progress and angle for arc/knob (remaining time, not elapsed)
-  const currentDuration =
-    dragging && previewDuration ? previewDuration : duration;
-  const currentTimeLeft =
-    dragging && previewDuration ? previewDuration : timeLeft;
-
   return (
     <WidgetBase
       style={{
         width: "100%",
-        padding: "0.7rem 0.3rem 0.7rem 0.3rem",
+        padding: "2rem 1rem",
         display: "flex",
-        flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
+        textAlign: "center",
       }}
       className={className}
     >
-      <TimerCircle
-        svgRef={svgRef}
-        currentTimeLeft={currentTimeLeft}
-        currentDuration={currentDuration}
-        dragging={dragging}
-        onPointerDown={onPointerDown}
-        formatTime={formatTime}
-      />
-      <TimerControls
-        isRunning={isRunning}
-        isPaused={isPaused}
-        onToggle={toggleTimer}
-        onReset={resetTimer}
-      />
+      <div
+        style={{
+          fontSize: "3.5rem",
+          fontWeight: "700",
+          fontStyle: "italic",
+          color: "rgb(252, 128, 159)",
+          padding: "0.3rem",
+        }}
+      >
+        Timer Coming Soon!
+      </div>
     </WidgetBase>
   );
 }
